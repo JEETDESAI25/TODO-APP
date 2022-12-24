eb54e1e3121536279c499f4c11b64482338087b2
JEETDESAI25 <jeetdesai25@gmail.com>
2022-12-23 17:00:42 -0800

    changes to wb app

--- TODO APP/web.py
+++ TODO APP/web.py
@@ -1,22 +1,26 @@
 import streamlit as st
-import functions 
+import functions
 
 todos = functions.get_todos()
+
 
 def add_todo():
     todo = st.session_state["new todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
+
 st.title("My Todo App")
-st.subheader("This is my Todo App")
-st.write("This app is to useful to increase your productivity")
-
-for todo in todos:
-    st.checkbox(todo)
-
-st.text_input(label="", placeholder="Add a Todo",on_change=add_todo, key="new todo")
+st.subheader("This app is useful to increase your productivity.")
+st.write("Here are your Todo's.")
 
 
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
-st.session_state
+st.text_input(label="", placeholder="Add a Todo", on_change=add_todo, key="new todo")
